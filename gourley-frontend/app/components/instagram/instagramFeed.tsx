2372b3c8750900483d459ad9d7b88037bd87ee9c
KParrish <kp@few.io>
2025-09-15 10:19:46 -0600

    updated buttons

---
+++ gourley-frontend/app/components/instagram/instagramFeed.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function InstagramFeed() {
+  return (
+    <div style={{ width: "100%", maxWidth: "800px", margin: "0 auto" }}>
+      <iframe
+        src="https://snapwidget.com/embed/123456" // replace with your link
+        className="snapwidget-widget"
+        allowTransparency={true}
+        frameBorder="0"
+        scrolling="no"
+        style={{
+          border: "none",
+          overflow: "hidden",
+          width: "100%",
+          height: "400px",
+        }}
+      ></iframe>
+    </div>
+  );
+}
